9e9c4f1743c1a260784fe5c75f97d620bd9114fd
jos <jos@testvm01.lan>
2025-01-14 22:12:05 +0100

    Added new Stock feature

--- stoecke/urls.py
+++ stoecke/urls.py
@@ -1,8 +1,10 @@
-# adressen/urls.py
+# stoecke/urls.py
 
 from django.urls import path
 from .views import StoeckeHome
+from .views import StockCreate
 
 urlpatterns = [
              path('', StoeckeHome.as_view(), name='stock_start'),
+             path('new/', StockCreate.as_view(), name='new_stock'),
              ]
--- stoecke/views.py
+++ stoecke/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 
 # Create your views here.
 
 from django.views.generic import ListView # Neu
+from django.views.generic import CreateView
 
 from .models import Stoecke # Neu
 
 class StoeckeHome(ListView): # Neu
     model = Stoecke # Neu
     template_name = 'stoecke/stock_home.html' # Neu
+
+class StockCreate(CreateView):
+    model = Stoecke 
+    template_name = 'stoecke/stock_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('stock_start')
